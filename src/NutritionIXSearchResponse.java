cf4bd7bd7af1c3ce579003b40c5ac4ae2aa1b8f1
WiseHollow <ieatlotz@gmail.com>
2017-05-16 16:50:19 -0400

    Moved classes as child static classes.

--- src/NutritionIXSearchResponse.java
+++ src/NutritionIXSearchResponse.java
@@ -1,5 +1,8 @@
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by John on 5/16/2017.
@@ -17,5 +20,50 @@ public class NutritionIXSearchResponse
 
     public ItemsResponseBody getBodyDetails() { return body; }
     public ResponseHeaders getHeaderDetails() { return headers; }
+
+    /**
+     * Created by John on 5/16/2017.
+     */
+    static class ItemsResponseBody
+    {
+        private NutritionIXItem[] items;
+
+        public ItemsResponseBody(JsonNode body)
+        {
+            try
+            {
+                JSONArray hits = body.getObject().getJSONArray("hits");
+                if (hits.length() == 0)
+                {
+                    System.out.println("Search returned no items.");
+                    return;
+                }
+                items = new NutritionIXItem[hits.length()];
+                for (int i = 0; i < hits.length(); i++)
+                {
+                    JSONObject jsonItem = hits.getJSONObject(i);
+                    if (jsonItem == null)
+                        continue;
+                    JSONObject fields = jsonItem.getJSONObject("fields");
+
+                    String id = fields.getString("item_id");
+                    String name = fields.getString("item_name");
+                    String brand = fields.getString("brand_name");
+
+                    int calories = fields.getInt("nf_calories");
+
+                    items[i] = new NutritionIXItem(id, name, brand, calories);
+                }
+            } catch (JSONException e)
+            {
+                e.printStackTrace();
+                return;
+            }
+
+
+        }
+
+        public NutritionIXItem[] getItems() { return items; }
+    }
 }
 
--- src/NutritionIxGetItemResponse.java
+++ src/NutritionIxGetItemResponse.java
@@ -1,5 +1,7 @@
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by John on 5/16/2017.
@@ -17,5 +19,38 @@ public class NutritionIxGetItemResponse
 
     public ItemResponseBody getBodyDetails() { return body; }
     public ResponseHeaders getHeaderDetails() { return headers; }
+
+    /**
+     * Created by John on 5/16/2017.
+     */
+    static class ItemResponseBody
+    {
+        private NutritionIXItem item;
+
+        public ItemResponseBody(JsonNode body)
+        {
+            try
+            {
+                JSONObject jsonItem = body.getObject();
+                if (jsonItem == null)
+                    return;
+                JSONObject fields = jsonItem.getJSONObject("fields");
+
+                String id = fields.getString("item_id");
+                String name = fields.getString("item_name");
+                String brand = fields.getString("brand_name");
+
+                int calories = fields.getInt("nf_calories");
+
+                item = new NutritionIXItem(id, name, brand, calories);
+            } catch (JSONException e)
+            {
+                e.printStackTrace();
+                return;
+            }
+        }
+
+        public NutritionIXItem getItem() { return item; }
+    }
 }
 
