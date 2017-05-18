ff863c9b8f37a15abfb55f98c457067a6bb3e6ed
John <ieatlotz@gmail.com>
2017-05-17 21:36:01 -0400

    Commit of the night.

--- src/Test.java
+++ src/Test.java
@@ -14,18 +14,18 @@ public class Test
         NutritionIXGetItemResponse response = new NutritionIXGetItemResponse(node);
         System.out.println(response.getBodyDetails().getItem());
 
-        //query = new NutritionIXQuery(NutritionIXQuery.NutritionIXQueryType.SEARCH);
-        //query.addArgument("cheddar cheese")
-        //        .addField(NutritionIXField.ITEM_NAME)
-        //        .addField(NutritionIXField.ITEM_ID)
-        //        .addField(NutritionIXField.ITEM_BRAND)
-        //        .addField(NutritionIXField.SERVING_SIZE)
-        //        .addField(NutritionIXField.CALORIES)
-        //        .addField(NutritionIXField.TOTAL_FAT);
-        //        //.addField("item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat%2Cnf_serving_size_unit");
-        //node = query.runQuery();
-        //NutritionIXSearchResponse searchResponse = new NutritionIXSearchResponse(node);
-        //for (NutritionIXItem item : searchResponse.getBodyDetails().getItems())
-        //    System.out.println(item);
+        query = new NutritionIXQuery(NutritionIXQuery.NutritionIXQueryType.SEARCH);
+        query.addArgument("cheddar cheese")
+                .addField(NutritionIXField.ITEM_NAME)
+                .addField(NutritionIXField.ITEM_ID)
+                .addField(NutritionIXField.ITEM_BRAND)
+                .addField(NutritionIXField.SERVING_SIZE)
+                .addField(NutritionIXField.CALORIES)
+                .addField(NutritionIXField.TOTAL_FAT);
+                //.addField("item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat%2Cnf_serving_size_unit");
+        node = query.runQuery();
+        NutritionIXSearchResponse searchResponse = new NutritionIXSearchResponse(node);
+        for (NutritionIXItem item : searchResponse.getBodyDetails().getItems())
+            System.out.println(item);
     }
 }
