095eb31e8c73d4efabf38ce44af2fcb0e638eb40
WiseHollow <ieatlotz@gmail.com>
2017-05-25 15:44:14 -0400

    Trimmed main.java.

--- src/net/johnbrooks/NW/Main.java
+++ src/net/johnbrooks/NW/Main.java
@@ -1,8 +1,5 @@
 package net.johnbrooks.NW;
 
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,27 +11,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //net.johnbrooks.NW.NutritionIXQuery query = new net.johnbrooks.NW.NutritionIXQuery(net.johnbrooks.NW.NutritionIXQuery.NutritionIXQueryType.GET);
-        //query.addArgument("51c3d78797c3e6d8d3b546cf");
-        //HttpResponse<JsonNode> node = query.runQuery();
-        //net.johnbrooks.NW.NutritionIXGetItemResponse response = new net.johnbrooks.NW.NutritionIXGetItemResponse(node);
-//
-        //System.out.println(response.getBodyDetails().getItem());
-//
-        //query = new net.johnbrooks.NW.NutritionIXQuery(net.johnbrooks.NW.NutritionIXQuery.NutritionIXQueryType.SEARCH);
-        //query.addArgument("cheddar cheese")
-        //        .addField(net.johnbrooks.NW.NutritionIXField.ITEM_NAME)
-        //        .addField(net.johnbrooks.NW.NutritionIXField.ITEM_ID)
-        //        .addField(net.johnbrooks.NW.NutritionIXField.ITEM_BRAND)
-        //        .addField(net.johnbrooks.NW.NutritionIXField.SERVING_SIZE)
-        //        .addField(net.johnbrooks.NW.NutritionIXField.CALORIES)
-        //        .addField(net.johnbrooks.NW.NutritionIXField.TOTAL_FAT);
-        //node = query.runQuery();
-//
-        //net.johnbrooks.NW.NutritionIXSearchResponse searchResponse = new net.johnbrooks.NW.NutritionIXSearchResponse(node);
-        //for (net.johnbrooks.NW.NutritionIXItem item : searchResponse.getBodyDetails().getItems())
-        //    System.out.println(item);
-
         String searchFor;
 
         try
